d24be0e15644e8ae6b21c07958219f03ceb84244
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-03-23 09:08:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Profoundry::Application.config.secret_key_base = 'cd640c0d8811c8d8b4acdc94a12843fb7ef8e320f87b26548deac6e8ad67e194bdbe6a8f168a2747a806d0d2ad558ef450e1d39a06994591bc2217c18a7597c4'
