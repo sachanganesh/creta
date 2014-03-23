d8dfb6228c41e2e65b1b20c16a2f416e8dfd700f
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-03-23 13:14:25 -0700

    Create articles controller and index, show actions

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  def index # Show list of articles
+    @articles = Articles.all
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show # Show individual articles
+    @article = Articles.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Profoundry::Application.routes.draw do
+  get "articles/index"
+  get "articles/create"
+  get "articles/new"
+  get "articles/edit"
+  get "articles/show"
+  get "articles/update"
+  get "articles/destroy"
   resources :articles
 
   root "pages#index"
