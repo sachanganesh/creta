dd3c4a4a50f6fed4dda0bd0f0c00ad2402c48a07
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-03-25 18:25:42 -0700

    Add migration for devise + oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324001733) do
+ActiveRecord::Schema.define(version: 20140326012516) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20140324001733) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
