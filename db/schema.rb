13dd3c0efc84b442397602a272c790dee6510eeb
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-04-22 11:52:38 -0700

    Add unconfirmed email mig and extra styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422013437) do
+ActiveRecord::Schema.define(version: 20140422184703) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20140422013437) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
