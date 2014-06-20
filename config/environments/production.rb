8a00d6b0fde66ce41f771c4fbc7b070386ad052d
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-06-19 22:14:39 -0700

    Mandrill smtp fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Creta::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrill.com",
+    :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 25 and 2525 are also supported with STARTTLS
-    :domain => 'creta.herokuapp.com',
+    #:domain => 'creta.herokuapp.com',
     :authentication => 'plain',
     :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => ENV['USERNAME'],
